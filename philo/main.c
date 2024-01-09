dd5d2074673ca22fc238c51f90ee582a33219c31
Juan Antonio <juanantonio@Mac-mini-de-Juan.local>
2024-01-09 16:22:34 +0100

    first steps philo v2.0

--- philo/main.c
+++ philo/main.c
@@ -6,29 +6,34 @@
 /*   By: juan-anm  <juan-anm@student.42barcel>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 11:59:37 by juan-anm          #+#    #+#             */
-/*   Updated: 2024/01/09 14:09:19 by juan-anm         ###   ########.fr       */
+/*   Updated: 2024/01/09 15:50:53 by juan-anm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-t_table table;
-
 int	main(int ac, char **av)
 {
+	t_table	table;
+
 	if (ac != 5 && ac != 6)
-		return(ft_error_msg(1));
-	if (ft_parse_arg(&table, av))
-		return(2);
+		return (ft_error_msg(1));
+	if (ft_check_input(av))
+		return (ft_error_msg(2));
+	if (ft_init_table(&table, av))
+		return (2);
 
 }
 
-int	ft_parse_arg(t_table *table, char **av)
+int	ft_init_table(t_table *table, char **av)
 {
-	(void) table;
-	
-	if (ft_check_input(av))
-		return(ft_error_msg(2));
+	table->num_phil = ft_atol(av[1]);
+	table->time_2die = ft_atol(av[2]);
+	table->time_2eat = ft_atol(av[3]);
+	table->time_2sleep = ft_atol(av[4]);
+	if (av[5])
+		table->num_meals = ft_atol(av[5]);
+
 	return (0);
 }
 
@@ -55,8 +60,36 @@ int	ft_check_input(char **av)
 int	ft_error_msg(int code)
 {
 	if (code == 1)
-		printf("Error: Wrong number of arguments");
+		printf("Error: Wrong number of arguments\n");
 	if (code == 2)
-		printf("Error: Wrong arguments format");
-	return(code);
+		printf("Error: Wrong arguments format, please insert a positive int\n");
+	return (code);
+}
+
+long	ft_atol(const char *str)
+{
+	int		i;
+	long	ato;
+	int		neg;
+
+	ato = 0;
+	neg = 0;
+	i = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			neg++;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		ato = ato * 10;
+		ato = ato + (str[i] - '0');
+		i++;
+	}
+	if (neg == 1)
+		ato = ato * -1;
+	return (ato);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: juan-anm  <juan-anm@student.42barcel>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 11:41:15 by juanantonio       #+#    #+#             */
-/*   Updated: 2024/01/09 14:09:32 by juan-anm         ###   ########.fr       */
+/*   Updated: 2024/01/09 15:33:42 by juan-anm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,17 +30,17 @@ typedef struct s_philo
 
 typedef struct s_table
 {
-	int	num_phil;
-	int	time_2die;
-	int	time_2eat;
-	int	time_2sleep;
-	int	num_meals;
+	long	num_phil;
+	long	time_2die;
+	long	time_2eat;
+	long	time_2sleep;
+	long	num_meals;
 
 }	t_table;
 
 int		ft_error_msg(int code);
-int		ft_parse_arg(t_table *table, char **av);
+int		ft_init_table(t_table *table, char **av);
 int		ft_check_input(char **av);
-
+long	ft_atol(const char *str);
 
 #endif
